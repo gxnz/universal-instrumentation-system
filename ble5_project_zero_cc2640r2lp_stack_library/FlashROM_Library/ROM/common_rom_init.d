c3b9d07978a9e5904b569533088202c8db89308f
gxnz <georgexnz@gmail.com>
2020-05-29 17:05:49 +1200

    Final

---
+++ ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/ROM/common_rom_init.d
@@ -0,0 +1,5 @@
+# FIXED
+
+ROM/common_rom_init.obj: C:/ti/simplelink_cc2640r2_sdk_3_20_00_21/source/ti/ble5stack/rom/r2/common_rom_init.c
+
+C:/ti/simplelink_cc2640r2_sdk_3_20_00_21/source/ti/ble5stack/rom/r2/common_rom_init.c: 
